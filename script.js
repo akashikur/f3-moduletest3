3b0de7a64adba585932b1b9913935e11c95aa05e
Akash M <108210779+akashikur@users.noreply.github.com>
2023-08-05 22:04:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+async function getIpAddress() {
+  try {
+    const response = await fetch("https://api.ipify.org?format=json");
+    const data = await response.json();
+    const ipAddress = data.ip;
+    document.getElementById(
+      "ipaddress"
+    ).innerText = `Your Current IP Address is ${ipAddress}`;
+  } catch (error) {
+    console.error("Error fetching IP address:", error);
+    document.getElementById("ipAddress").textContent =
+      "Error fetching IP address";
+  }
+}
+
+getIpAddress();
